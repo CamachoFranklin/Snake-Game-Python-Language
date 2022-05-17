64af9b60ab546efe675301e63c9a656ec92af884
Franklin Camacho <camachof44@gmail.com>
2022-05-16 20:16:33 -0400

    Agregando movimientos a la serpiente
    En este punto se agregó la función movimiento, la cual tiene las sentencias para los movimientos hacia: Arriba, abajo, derecha e izquierda. A su vez se agregó importó la librería time que será usada para agregar un delay al movimiento a la serpiente.

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,5 +1,9 @@
-# Importamos la librerias a utilizar
+# Importamos las librerias a utilizar
 import turtle
+import time
+
+# Se crea una variable que indicará el tiempo en que se moverá la serpiente
+retraso = 0.1
 
 # Se crea el lienzo donde estará corriendo el juego
 screen = turtle.Screen()
@@ -34,7 +38,56 @@ serpiente.direction = 'stop'
 # Se le asigna el color a la serpiente
 serpiente.color('green')
 
+# Se crea la función de movimiento
+def movimiento():
 
+    # Se crea la condición para el caso en que la serpiente se mueva hacia arriba
+    if serpiente.direction == 'up':
+
+        # La función ycord devuelve la coordenada de la serpiente en el eje Y
+        y = serpiente.ycord()
+
+        # La función sety mueve un objeto en el eje Y, a la posición actual se le suman 20 pixeles.
+        serpiente.sety(y + 20)
+
+    # Se crea la condición para el caso en que la serpiente se mueva hacia abajo
+    if serpiente.direction == 'down':
+
+        # La función ycord devuelve la coordenada de la serpiente en el eje Y
+        y = serpiente.ycord()
+
+        # La función sety mueve un objeto en el eje Y, a la posición actual se le restan 20 pixeles.
+        serpiente.sety(y - 20)
+
+    # Se crea la condición para el caso en que la serpiente se mueva hacia la derecha
+    if serpiente.direction == 'right':
+
+        # La función xcord devuelve la coordenada de la serpiente en el eje X
+        x = serpiente.xcord()
+
+        # La función setx mueve un objeto en el eje X, a la posición actual se le suman 20 pixeles.
+        serpiente.setx(x + 20)
+
+    # Se crea la condición para el caso en que la serpiente se mueva hacia arriba
+    if serpiente.direction == 'left':
+
+        # La función xcord devuelve la coordenada de la serpiente en el eje X
+        x = serpiente.xcord()
+
+        # La función setx mueve un objeto en el eje X, a la posición actual se le restan 20 pixeles.
+        serpiente.setx(x - 20)
+
+# Ciclo que actualiza la pantalla con cada movimiento de la serpiente
+while True:
+
+    # Actualizamos la pantalla con cada movimiento de la serpiente
+    screen.update()
+
+    # Se hace uso de la función movimiento
+    movimiento()
+
+    # Se agrega un delay al movimiento de la serpiente
+    time.sleep(retraso)
 
 # Función para mantener la ventana abierta
 turtle.done()
